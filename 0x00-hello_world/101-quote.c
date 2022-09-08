d09926cad5a8fd5207f4965b6dc834f92869032d
Stellamaris Nwafor <wendumaris@gmail.com>
2022-09-08 22:43:46 +0100

    C program that prints statement to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Print an error statement
+ * main - Print an error statement with standard error
  * Return: 1 (success)
  */
 
 int main(void)
 {
-	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
