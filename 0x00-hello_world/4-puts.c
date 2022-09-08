bf62c4ce11b7beca899ed8c1542ec83cc972b625
Stellamaris Nwafor <wendumaris@gmail.com>
2022-09-08 20:37:51 +0100

    Outputing using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building  multilingual puzzle");
 
-	return(0);
+	return (0);
 }
