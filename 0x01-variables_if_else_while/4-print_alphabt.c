4a2bacbef3535fc3149d09d8bfb6ce1e15882bdf
Stellamaris Nwafor <wendumaris@gmail.com>
2022-09-09 22:00:06 +0100

    Printing alphabets without two letters

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,8 +7,10 @@
 int main(void)
 {
 	char letter;
-	for (letter = 'a'; letter <= 'z'; letter++){
-		if (letter != 'q' || letter != 'e'){
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+		{
 			putchar(letter);
 		}
 	}
