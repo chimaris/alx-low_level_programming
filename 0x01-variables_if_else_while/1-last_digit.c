6c181a7bd785de9aa120e37620afa412bd7b1414
Stellamaris Nwafor <wendumaris@gmail.com>
2022-09-09 15:51:17 +0100

    Program to display last digit of number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,9 @@
 
 /* betty style doc for function main goes there */
 
-/* main - To get the last digit of a number stored in var n
+/**
+ * main - To get the last digit of a number stored in var n
+ *
  * Return: 0
  */
 
@@ -23,7 +25,8 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 	} else if (lastDigit < 6 && lastDigit != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+n, lastDigit);
 	} else
 	{
 		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
