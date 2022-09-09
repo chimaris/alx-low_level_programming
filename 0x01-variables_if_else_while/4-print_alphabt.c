0529f6e3e8a90a5b9850cacc40bd4b74651d4a05
Stellamaris Nwafor <wendumaris@gmail.com>
2022-09-09 21:56:24 +0100

    Printing alphabets with two letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints the alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++){
+		if (letter != 'q' || letter != 'e'){
+			putchar(letter);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
