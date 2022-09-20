d3edd34fe0a3489d22306639c28679d0a5f8777b
Stellamaris Nwafor <wendumaris@gmail.com>
2022-09-20 16:49:23 +0100

    fixed betty

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,7 +10,7 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	/** The standard library provides a similar function: strcpy. Run man strcpy */
+	/** This provides a similar function: strcpy. Run man strcpy */
 	int count = 0;
 
 	while (count >= 0)
