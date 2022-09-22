2ffbac91ca2c6c7c787388f57006915e84189ce7
Stellamaris Nwafor <wendumaris@gmail.com>
2022-09-23 00:19:41 +0100

    reverse string

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,14 +9,12 @@
 
 void reverse_array(int *a, int n)
 {
-	int i;
+	int i, temp;
 
-	for (i = n - 1; i >= 0; i--)
+	for (i = n - 1; i >= n / 2; i--)
 	{
-		printf("%d", a[i]);
-		if (i == 0)
-			continue;
-		printf(", ");
+		temp = a[n - 1 - i];
+		a[n - 1 - i] = a[i];
+		a[i] = temp;
 	}
-	printf("\n");
 }
