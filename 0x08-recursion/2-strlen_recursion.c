dd7478105e488c86e679fd208b0187ebddcbf894
Stellamaris Nwafor <wendumaris@gmail.com>
2022-09-28 21:58:16 +0100

    function that returns the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints a string in reverse
+ * @s: param 1
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
+	return (len);
+}
