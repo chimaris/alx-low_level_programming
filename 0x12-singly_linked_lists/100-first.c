097c9925c94aad4b9238cb9d0e004e17b948eac5
Stellamaris Nwafor <wendumaris@gmail.com>
2022-10-21 02:09:56 +0100

    function that prints before the main function is executed.

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void __attribute__((constructor)) maris(void);
+
+/**
+ * maris - Prints a string before the main function executed
+ */
+
+void maris(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
+}
