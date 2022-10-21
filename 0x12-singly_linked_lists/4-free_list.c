5d12b2b2bffd291d7e5874d1c41c808e195938fe
Stellamaris Nwafor <wendumaris@gmail.com>
2022-10-21 01:37:41 +0100

    function that frees a list_t list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list_t list.
+ * @head: pointer to the list_t list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while(head)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+
+}
