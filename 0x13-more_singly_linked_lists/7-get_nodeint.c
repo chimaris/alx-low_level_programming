9d76cf8da14354c0f9184c9f0369903442825a66
Stellamaris Nwafor <wendumaris@gmail.com>
2022-10-25 19:14:58 +0100

    return the nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,12 +11,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int node;
 
-	if (head == NULL)
-		return (NULL);
-
 	node = 0;
 	while (node < index)
 	{
+		if (head == NULL)
+			return (NULL);
 		head = head->next;
 		node++;
 	}
